881de82575a2a445ea8f548b8f0e43cfd29729f2
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-21 05:49:02 +0000

    required the body-parse library and used app.post but it is not fully functional

--- express_server.js
+++ express_server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http://www.google.com"
+  "9sm5xk": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -34,11 +39,17 @@ app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!'};
   res.render("hello_world", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
