3719591e05673a464668f074073cb5cfccd39b4c
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-24 20:00:08 +0000

    encrypt cookies and test it is workink. replace the cookie session

--- express_server.js
+++ express_server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const app = express();
 const PORT = 8080;
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'userId',
+  keys: ["olive", "fan"]
+}));
 app.set("view engine", "ejs");
 const generateRandomString = function() {
 
@@ -37,6 +41,12 @@ const urlForUser = function(id) {
   }
   return userLongURLs;
 };
+const hashedPassword = function(userPassword) {
+  return bcrypt.hashSync(userPassword,10);
+};
+const validatePassword = function(userPassword) {
+  return bcrypt.compareSync(userPassword, hashedPassword(userPassword));
+};
 
 app.post("/urls", (req, res) => {
   
@@ -46,12 +56,12 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = {};
   urlDatabase[shortURL]["longURL"] = longURL;
   // this only saves if the user is logged in, is it supposed to remember the user after logging out and in next time/
-  urlDatabase[shortURL]["user"] = req.cookies["user"];
+  urlDatabase[shortURL]["user"] = req.session.userId;
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userURL = urlForUser(req.cookies["user"]);
+  const userURL = urlForUser(req.session.userId);
   if (req.params.shortURL in userURL) {
     delete userURL[req.params.shortURL];
     res.redirect("/urls");
@@ -60,7 +70,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   }
 });
 app.post("/urls/:shortURL/edit", (req, res) => {
-  const userURL = urlForUser(req.cookies["user"]);
+  const userURL = urlForUser(req.session.userId);
   if (req.params.shortURL in userURL) {
     userURL[req.params.shortURL]["longURL"] = req.body.longURL;
     res.redirect("/urls");
@@ -70,7 +80,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 app.get("/urls/:shortURL/edit", (req, res) => {
 
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], user: req.cookies["user"]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]["longURL"], user: req.session.userId};
   res.render("urls_show", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
@@ -80,40 +90,41 @@ app.get("/u/:shortURL", (req, res) => {
 });
 // registration, login, logout
 app.post("/register", (req, res) => {
-  if (req.body.email === ""  || req.body.password === "") {
-    res.status(400).send("Enter email and password");
+  if (req.body.email === "" || req.body.password === "") {
+    res.render("error", {ErrorMessage: "Enter email and/or password", user: null});
   } else if (findUserByEmail(req.body.email)) {
-    res.status(400).send("You already have an account!");
+    res.render("error", {ErrorMessage: "You already have an account! Login instead",
+      user: findUserByEmail(req.body.email)});
   } else {
     const newID = generateRandomString();
     const user = {};
     users[newID] = user;
     user.id = newID;
     user.email = req.body.email;
-    user.password = req.body.password;
+    user.password = hashedPassword(req.body.password);
     console.log(users);
     
-
-    res.cookie("user", newID);
+    req.session.userId = newID;
     res.redirect("/urls");
   }
 });
 app.post("/login", (req, res) => {
   if (findUserByEmail(req.body.email)) {
-    console.log(req.cookies["user"]);
-    if (req.body.password === users[findUserByEmail(req.body.email).id]["password"]) {
-      res.cookie("user", findUserByEmail(req.body.email).id);
+    console.log(req.session.userId);
+    if (validatePassword(req.body.password)) {
+      req.session.userId = findUserByEmail(req.body.email).id;
       res.redirect("/urls");
     } else {
-      res.status(403).send("incorrect password!");
+      res.render("error", {ErrorMessage: "incorrect password!", user: findUserByEmail(req.body.email)});
     }
 
   } else {
-    res.status(403).send("Cannot find email!");
+    res.render("error", {ErrorMessage: "Cannot find email! You might need to register",
+      user: findUserByEmail(req.body.email)});
   }
 });
 app.post("/logout", (req, res) => {
-  res.clearCookie("user");
+  req.session = null;
   res.redirect("/urls");
 });
 app.get("/login", (req, res) => {
@@ -143,11 +154,11 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 app.get("/urls", (req, res) => {
-  const user = req.cookies["user"];
+  const user = req.session.userId;
   if (user) {
     const userURL = urlForUser(user);
     let templateVars = { urls: userURL,
-      user: req.cookies["user"]};
+      user: req.session.userId};
     res.render("urls_index", templateVars);
   } else {
     res.render("error", {ErrorMessage: "login or register, first", user: null});
@@ -155,24 +166,24 @@ app.get("/urls", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!',
-    user: req.cookies["user"]};
+    user: req.session.userId};
   res.render("hello_world", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  if (req.cookies["user"]) {
-    let templateVars = {user: req.cookies["user"]};
+  if (req.session.userId) {
+    let templateVars = {user: req.session.userId};
     res.render("urls_new", templateVars);
   }
   res.redirect("/login");
 });
 app.get("/urls/:shortURL", (req, res) => {
-  if (req.cookies["user"]) {
-    const userURL = urlForUser(req.cookies["user"]);
+  if (req.session.userId) {
+    const userURL = urlForUser(req.session.userId);
     if (req.params.shortURL in userURL) {
-      let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: req.cookies["user"]};
+      let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: req.session.userId};
       res.render("urls_show", templateVars);
     } else {
-      res.render("error", {ErrorMessage: "URL doesn't match the id", user: req.cookies["user"]});
+      res.render("error", {ErrorMessage: "URL doesn't match the id", user: req.session.userId});
     }
   } else {
     res.render("error", {ErrorMessage: "login or register, first", user: null});
